39f2e193d481eaaa023c08023cd8d51bcc9663f6
adonozo <donozo.alvaro@gmail.com>
2021-07-25 12:22:13 +0100

    Implement GetCategories on controller

--- MMTShop/MMTShop-Service/src/api/MMT.Shop.Api/Controllers/CategoryController.cs
+++ MMTShop/MMTShop-Service/src/api/MMT.Shop.Api/Controllers/CategoryController.cs
@@ -1,20 +1,38 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MMT.Shop.ServiceInterfaces;
 
 namespace MMT.Shop.Api.Controllers
 {
-    public class CategoryController
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class CategoryController : ControllerBase
     {
+        private readonly ILogger<CategoryController> logger;
+        private readonly ICategoryService categoryService;
+        
         public CategoryController(ILogger<CategoryController> logger, ICategoryService service)
         {
-            throw new System.NotImplementedException();
+            this.logger = logger;
+            this.categoryService = service;
         }
 
+        [HttpGet]
         public async Task<IActionResult> GetCategories()
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var result = await this.categoryService.GetCategories();
+                return this.Ok(result);
+            }
+            catch (Exception exception)
+            {
+                this.logger.LogError(exception, "Could not get the categories list");
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
--- MMTShop/MMTShop-Service/test/api/MMT.Shop.Api.Test/CategoryControllerTest.cs
+++ MMTShop/MMTShop-Service/test/api/MMT.Shop.Api.Test/CategoryControllerTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -8,6 +9,7 @@ using MMT.Shop.Api.Controllers;
 using MMT.Shop.Models;
 using MMT.Shop.ServiceInterfaces;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Xunit;
 
 namespace MMT.Shop.Api.Test
@@ -29,6 +31,23 @@ namespace MMT.Shop.Api.Test
             // Assert
             result.Should().BeOfType<OkObjectResult>();
             ((OkObjectResult) result).StatusCode.Should().Be(StatusCodes.Status200OK);
+        }
+
+        [Fact]
+        public async Task GetCategories_OnInvalidRequest_ShouldReturnInternalError()
+        {
+            // Arrange
+            var logger = Substitute.For<ILogger<CategoryController>>();
+            var service = Substitute.For<ICategoryService>();
+            service.GetCategories().Throws(_ => throw new Exception());
+            var controller = new CategoryController(logger, service);
+            
+            // Act
+            var result = await controller.GetCategories();
+
+            // Assert
+            result.Should().BeOfType<StatusCodeResult>();
+            ((StatusCodeResult) result).StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
         }
     }
 }
